5443dc7d1e05d2a3d538bdfffb9701f05f88c4f4
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-19 16:57:41 +0200

    delete products method added

--- ej2/index.js
+++ ej2/index.js
@@ -15,10 +15,16 @@ app.get("/products", (req, res) => {
   res.status(200).send({ message: "Products list" });
 });
 
-// '/products' route; PUT method
+// '/products/:id' route; PUT method
 app.put("/products/:id", (req, res) => {
   const productId = +req.params.id;
   res.status(200).send({ message: `Update product ${productId}` });
+});
+
+// '/products/:is' route; DELETE method
+app.delete("/products/:id", (req, res) => {
+  const productId = +req.params.id;
+  res.status(200).send({ message: `Product ${productId} deleted` });
 });
 
 app.listen(PORT, () => {
