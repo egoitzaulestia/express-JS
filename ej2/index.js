4619142e14cf70f78a71170faa3bb2d29b2faf32
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-19 17:05:31 +0200

    post products method created

--- ej2/index.js
+++ ej2/index.js
@@ -5,23 +5,31 @@ const PORT = 3000;
 
 app.use(express.json());
 
-// '/' (root) route; GET method
+// '/' (root) GET method route
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Welcome to our shop!" });
 });
 
-// '/products' route; GET method
+// '/products' GET method route
 app.get("/products", (req, res) => {
   res.status(200).send({ message: "Products list" });
 });
 
-// '/products/:id' route; PUT method
+// '/products/:id' POST methed route
+app.post("/products/:id", (req, res) => {
+  const newProduct = req.body;
+  res
+    .status(201)
+    .send({ message: `New product created: ${newProduct.product}` });
+});
+
+// '/products/:id' PUT method route
 app.put("/products/:id", (req, res) => {
   const productId = +req.params.id;
   res.status(200).send({ message: `Update product ${productId}` });
 });
 
-// '/products/:is' route; DELETE method
+// '/products/:is' DELETE method route
 app.delete("/products/:id", (req, res) => {
   const productId = +req.params.id;
   res.status(200).send({ message: `Product ${productId} deleted` });
