d8ba5b055b0c56d501588f0b7b1ce1f66d0675d5
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-19 16:47:55 +0200

    root and products routes added

--- ej2/index.js
+++ ej2/index.js
@@ -5,6 +5,16 @@ const PORT = 3000;
 
 app.use(express.json());
 
+// Root route
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Welcome to our shop!" });
+});
+
+// Products root
+app.get("/products", (req, res) => {
+  res.status(200).send({ message: "Products list" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
